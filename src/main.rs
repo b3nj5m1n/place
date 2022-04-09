93c89e0804eab896fe0c26fd948be832430403b2
b3nj4m1n <b3nj4m1n@gmx.net>
2022-04-09 14:03:55 +0200

    Parser

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,165 @@
 use std::error::Error;
-use std::str::FromStr;
 use std::io;
 use std::process;
+use std::str::FromStr;
 
+use chrono::prelude::*;
+use serde::de::Error as SerdeError;
+use serde::de::Visitor;
 use serde::{Deserialize, Deserializer, Serialize};
 
-#[derive(Debug, Deserialize)]
-struct PixelPlacement2017 {
-    #[serde(rename = "ts")]
-    timestamp: String,
-    user_hash: String,
-    #[serde(rename = "x_coordinate")]
-    coordinate_x: u16,
-    #[serde(rename = "y_coordinate")]
-    coordinate_y: u16,
-    color: u8,
+struct PixelPlacementVisotor {}
+
+impl<'de> Visitor<'de> for PixelPlacementVisotor {
+    type Value = PixelPlacement;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("Could not deserialize pixel placement")
+    }
+
+    fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+    where
+        A: serde::de::MapAccess<'de>,
+    {
+        let mut timestamp: i64 = i64::MAX;
+        let mut user_hash: String = String::from("");
+        let mut coordinate_x: u16 = u16::MAX;
+        let mut coordinate_y: u16 = u16::MAX;
+        let mut color: String = String::from("");
+        let mut year: PlacementYear = PlacementYear::_UNKOWN;
+
+        while let Some((key, value)) = map.next_entry::<String, String>()? {
+            match key.as_str() {
+                "timestamp" | "ts" => {
+                    timestamp = value.parse::<DateTime<Utc>>().unwrap().timestamp();
+                }
+                "user_id" | "user_hash" => {
+                    user_hash = value;
+                }
+                "pixel_color" => {
+                    color = value;
+                    year = PlacementYear::_2022;
+                }
+                "color" => {
+                    match u16::from_str(&value) {
+                        Ok(value) => {
+                            match value {
+                                0 => { color = String::from("#FFFFFF"); }
+                                1 => { color = String::from("#E4E4E4"); }
+                                2 => { color = String::from("#888888"); }
+                                3 => { color = String::from("#222222"); }
+                                4 => { color = String::from("#FFA7D1"); }
+                                5 => { color = String::from("#E50000"); }
+                                6 => { color = String::from("#E59500"); }
+                                7 => { color = String::from("#A06A42"); }
+                                8 => { color = String::from("#E5D900"); }
+                                9 => { color = String::from("#94E044"); }
+                                10 => { color = String::from("#02BE01"); }
+                                11 => { color = String::from("#00E5F0"); }
+                                12 => { color = String::from("#0083C7"); }
+                                13 => { color = String::from("#0000EA"); }
+                                14 => { color = String::from("#E04AFF"); }
+                                15 => { color = String::from("#820080"); }
+                                _ => { return Err(A::Error::invalid_value(serde::de::Unexpected::Unsigned(value.into()), &"index between 0 and 15")); }
+                            }
+                        }
+                        Err(error) => {
+                            return Err(A::Error::custom(error));
+                        }
+                    }
+                    year = PlacementYear::_2017;
+                }
+                "coordinate" => {
+                    let nums = value.split(",").collect::<Vec<&str>>();
+                    if nums.len() != 2 {
+                        return Err(serde::de::Error::invalid_length(3, &"2"));
+                    }
+                    match u16::from_str(nums[0]) {
+                        Ok(value) => {
+                            coordinate_x = value;
+                        }
+                        Err(error) => {
+                            return Err(A::Error::custom(error));
+                        }
+                    }
+                    match u16::from_str(nums[1]) {
+                        Ok(value) => {
+                            coordinate_y = value;
+                        }
+                        Err(error) => {
+                            return Err(A::Error::custom(error));
+                        }
+                    }
+                    year = PlacementYear::_2022;
+                }
+                "x_coordinate" => {
+                    match u16::from_str(&value) {
+                        Ok(value) => {
+                            coordinate_x = value;
+                        }
+                        Err(error) => {
+                            return Err(A::Error::custom(error));
+                        }
+                    }
+                    year = PlacementYear::_2017;
+                }
+                "y_coordinate" => {
+                    match u16::from_str(&value) {
+                        Ok(value) => {
+                            coordinate_y = value;
+                        }
+                        Err(error) => {
+                            return Err(A::Error::custom(error));
+                        }
+                    }
+                    year = PlacementYear::_2017;
+                }
+                _ => {}
+            }
+        }
+
+        Ok(PixelPlacement {
+            timestamp,
+            user_hash,
+            coordinates: Coordinates {
+                x: coordinate_x,
+                y: coordinate_y,
+            },
+            color,
+            year,
+        })
+    }
 }
+
+#[derive(Debug)]
+struct PixelPlacement {
+    timestamp: i64,
+    user_hash: String,
+    coordinates: Coordinates,
+    color: String,
+    year: PlacementYear,
+}
+
 #[derive(Debug)]
 struct Coordinates {
     x: u16,
     y: u16,
 }
-fn deserialize_coordinates<'de, D>(deserializer: D) -> Result<Coordinates, D::Error>
-where D: Deserializer<'de> {
-    let buf = String::deserialize(deserializer)?;
-    let nums = buf.split(",").collect::<Vec<&str>>();
-    if nums.len() != 2 { return Err(serde::de::Error::invalid_length(3, &"2")); }
-    return Ok(Coordinates { x: u16::from_str(nums[0]).unwrap(), y: u16::from_str(nums[1]).unwrap() });
+
+#[derive(Debug)]
+enum PlacementYear {
+    _UNKOWN,
+    _2017,
+    _2022,
 }
-#[derive(Debug, Deserialize)]
-struct PixelPlacement2022 {
-    timestamp: String,
-    #[serde(rename = "user_id")]
-    user_hash: String,
-    #[serde(rename = "coordinate")]
-    #[serde(deserialize_with = "deserialize_coordinates")]
-    coordinates: Coordinates,
-    #[serde(rename = "pixel_color")]
-    color: String,
-}
-#[derive(Debug, Deserialize)]
-#[serde(untagged)]
-enum PixelPlacement {
-    Place2022(PixelPlacement2022),
-    Place2017(PixelPlacement2017),
+
+impl<'de> Deserialize<'de> for PixelPlacement {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_map(PixelPlacementVisotor {})
+    }
 }
 
 fn read() {
